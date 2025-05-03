85fd8fa4308fb548aa0a67ab48d0ef119c01f641
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-03 22:14:15 +0100

    add Description component for enhanced user guidance

---
+++ src/components/Description.tsx
@@ -0,0 +1,21 @@
+function Description() {
+  return (
+    <section className="w-full md:w-[60%] lg:w-[40%]">
+      <h1 className="text-[40px]/[60px] pb-[40px] font-bold">
+        Lock Down Your
+        <span>Security</span>
+        <br />
+        Generate
+        <span className="underlined underline-mask">Uncrackable</span> Passwords
+        <span className="underlined underline-overflow">in Seconds</span>
+      </h1>
+      <p className="font-medium">
+        FortiLock is your ultimate safeguard, effortlessly generating strong,
+        random passwords to elevate your digital security and keep your accounts
+        impenetrable
+      </p>
+    </section>
+  );
+}
+
+export default Description;
