6eb3219bab1d8419b4cacb9413c155966a7d135a
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-04 22:50:49 +0100

    fix: correct className formatting in Description component for consistent styling

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,12 +1,12 @@
 function Description() {
   return (
     <section className="w-full flex flex-col gap-[20px] text-center  lg:gap-[50px] lg:text-left lg:w-[47%]">
-      <h1 className="text-[30px] lg:text-[40px]/[60px] pb-[40px] font-bold">
+      <h1 className="text-[30px] lg:text-[40px]/[60px] lg:pb-[40px] font-bold">
         Lock Down Your Security
         <br />
         Generate Uncrackable Passwords in Seconds
       </h1>
-      <p className="font-medium">
+      <p className="font-medium lg: text-xl">
         FortiLock is your ultimate safeguard, effortlessly generating strong,
         random passwords to elevate your digital security and keep your accounts
         impenetrable
