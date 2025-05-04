ca0c888e5144537e76418ead7f32f7796f041c9c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-04 22:26:06 +0100

    fix: improve layout and text structure in Description and Main components

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,13 +1,10 @@
 function Description() {
   return (
-    <section className="w-full  lg:w-[40%]">
+    <section className="w-full flex flex-col gap-[20px] text-center  lg:gap-[50px] lg:text-left lg:w-[47%]">
       <h1 className="text-[30px] lg:text-[40px]/[60px] pb-[40px] font-bold">
-        Lock Down Your
-        <span>Security</span>
+        Lock Down Your Security
         <br />
-        Generate
-        <span className="underlined underline-mask">Uncrackable</span> Passwords
-        <span className="underlined underline-overflow">in Seconds</span>
+        Generate Uncrackable Passwords in Seconds
       </h1>
       <p className="font-medium">
         FortiLock is your ultimate safeguard, effortlessly generating strong,
