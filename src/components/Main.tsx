da4c909896fc61121fcab586c276babc7309f0f8
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-04 21:45:59 +0100

    fix: update flex direction in Main component for better responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import Generator from "./GeneratorApp";
 
 function Main() {
   return (
-    <div className="p-[30px] flex flex-col md:flex-row justify-between md:p-[50px] gap-[60px] items-center bg-[#EBEDDF] xl:px-[120px] :py-[100px]">
+    <div className="p-[30px] flex flex-col lg:flex-row justify-between md:p-[50px] gap-[60px] items-center bg-[#EBEDDF] xl:px-[120px] :py-[100px]">
       <Description />
       <Generator />
     </div>
