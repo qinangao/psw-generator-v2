6a9e96390bc9fe4b7e9873939fa9800da5bb039f
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-05 17:34:44 +0100

    feat: add Modal component for user input with optional confirm password

---
+++ src/components/Modal.tsx
@@ -0,0 +1,51 @@
+type ModalProps = {
+  title: string;
+  method: string;
+  password: string;
+  buttontext1: string;
+  showConfirmPassword?: boolean;
+  onClose?: () => void;
+};
+
+import Button from "./Button";
+function Modal({
+  title,
+  method,
+  password,
+  buttontext1,
+  showConfirmPassword = false,
+  onClose,
+}: ModalProps) {
+  return (
+    <div className="w-[80%] md:w-[50%] lg:w-[25%] rounded-[30px] z-100 p-[15px] bg-white border-2 border-black fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <h2 className="text-center text-2xl">{title}</h2>
+      <form className="flex flex-col gap-4 p-4">
+        <input
+          type={method}
+          placeholder={method}
+          className="border border-gray-300 rounded p-2"
+        />
+        <input
+          type={password}
+          placeholder={password}
+          className="border border-gray-300 rounded p-2"
+        />
+        {showConfirmPassword && (
+          <input
+            type="password"
+            placeholder="comfirm password"
+            className="border border-gray-300 rounded p-2"
+          />
+        )}
+        <div className="flex justify-between mt-4">
+          <Button>{buttontext1}</Button>
+          <Button bgColor="#ebeddf" textColor="#97b002" onClick={onClose}>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Modal;
